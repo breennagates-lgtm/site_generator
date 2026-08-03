ae32a3014a8d0637846d930b2498f7afecaf0ee5
breennagates-lgtm <breenna.gates@gmail.com>
2026-08-03 10:03:48 -0500

    Configure build script and docs directory for GitHub Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ from extract_title import extract_title
 
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from '{from_path}' to '{dest_path}' using '{template_path}'.")
     with open(from_path,"r") as f:
         md_contents = f.read()
@@ -16,6 +16,9 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md_contents)
 
     full_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    full_html = full_html.replace('href="/',f'href="{basepath}')
+    full_html = full_html.replace('src="/', f'src="{basepath}')
+    
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir != "":
@@ -24,14 +27,14 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as h:
      h.write(full_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     items =  os.listdir(dir_path_content)
     for item in items:
         item_full_path = os.path.join(dir_path_content, item)
         if os.path.isfile(item_full_path):
            html_filename = item.replace (".md", ".html")
            item_dest_path = os.path.join(dest_dir_path, html_filename)
-           generate_page(item_full_path, template_path,item_dest_path)
+           generate_page(item_full_path, template_path,item_dest_path, basepath)
         else:
            item_dest_path = os.path.join(dest_dir_path, item)
-           generate_pages_recursive(item_full_path, template_path, item_dest_path)
+           generate_pages_recursive(item_full_path, template_path, item_dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ from copy_contents import copy_contents_of_directory
 import shutil
 from generate_page import generate_pages_recursive
 import os
+import sys
+
 
 def main():
+    #check commmand to see what directory to use?
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
+
+
     # Create a TextNode instance
     text_node = TextNode("Hello, World!", text_type=TextType.BOLD, url="https://example.com")
 
@@ -13,7 +23,7 @@ def main():
 
 
     source_dir = "./static"
-    destination_dir = "./public" 
+    destination_dir = "./docs" 
 
     #deleting public directory if it exists
     if os.path.exists(destination_dir):
@@ -25,7 +35,7 @@ def main():
     print(f"Copying files from'{source_dir}' to '{destination_dir}'")
     copy_contents_of_directory(source_dir, destination_dir)
 
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
    
 
 main()
